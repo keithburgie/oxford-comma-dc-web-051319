29492159e7bff04905c2c1d2b824981da3e70298
Keith Burgie <krburgie@gmail.com>
2019-04-27 05:18:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+array = ["fiddleheads","okra","kohlrabi"]
 
+def oxford_comma(array)
+  if array.length == 1
+    return array.join("")
+  elsif
+    array.length == 2
+    return array.join(" and ")
+  elsif
+    array.length >= 3
+    last_item = ", and #{array.pop}"
+    return array.join(", ") + last_item
+  end
 end
